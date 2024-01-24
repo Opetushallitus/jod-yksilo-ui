91e622c775ef8462bd56d4535477d9fcbe446bfd
Sauli Anto <sauli.anto@gofore.com>
2024-01-24 17:16:48 +0200

    OPHJOD-131: Add Vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Renders the main page', () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
