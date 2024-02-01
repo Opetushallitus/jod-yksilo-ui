1e7b5ccbc5bf79bee79bea22a3c98c30af523b14
Sami Korpela <sami.korpela@gofore.com>
2024-02-01 09:11:08 +0200

    OPHJOD-162: Add tests for Button and fix Storybook preview

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'jod-base': '#444bac',
+        'jod-primary': '#f2a93b',
+        'jod-secondary': '#AFB3F3',
+        'jod-dark': '#181818',
+        'jod-light': '#f5f5f5',
+        'jod-white': '#ffffff',
+        'jod-black': '#000000',
+        'jod-disabled': '#cccccc',
+      },
+    },
   },
   plugins: [],
 };
