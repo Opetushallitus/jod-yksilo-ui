2b5839b25051b42255da5ac7d25c941b6b410df2
Sami Korpela <sami.korpela@gofore.com>
2024-02-13 13:48:59 +0200

    OPHJOD-153: Move counter example for better structure

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import counterReducer from '../routes/Home/counterSlice';
+import counterReducer from '../features/counter/counterSlice';
 
 const persistConfig = {
   key: 'root',
