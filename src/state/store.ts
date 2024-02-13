7c17603cabb7814a05ca6471c01b0423c60874da
Sami Korpela <sami.korpela@gofore.com>
2024-02-13 13:24:01 +0200

    OPHJOD-153: Add redux-persist

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import counterReducer from '../routes/Home/counterSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
