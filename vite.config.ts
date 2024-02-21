743de2ecd515fcf587aaf419f67510dc8e72a384
Sauli Anto <sauli.anto@gofore.com>
2024-02-21 12:00:10 +0200

    OPHJOD-200: Add API proxy to development server

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,13 @@ export default defineConfig({
       },
     ],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        xfwd: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
