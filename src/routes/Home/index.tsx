27c8ce5b1f0fabd31d2b9ae0ab24b9e842d04f77
Sami Korpela <sami.korpela@gofore.com>
2024-04-09 11:44:55 +0300

    OPHJOD-310: Update frontpage, Home, to match design

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,15 +1,33 @@
 import { useTranslation } from 'react-i18next';
-import { Counter } from '@/features';
 import { Title } from '@/components';
+import { HeroCard } from '@jod/design-system';
 
 const Home = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const profileIndexLink = `/${i18n.language}/${t('slugs.profile.index')}`;
 
   return (
     <div>
-      <Title value={t('home')} />
-      <h1>{t('home')}</h1>
-      <Counter />
+      <Title value={t('home.title')} />
+      <div
+        className={`mx-auto flex flex-col gap-[104px] bg-[url('https://images.unsplash.com/photo-1523464862212-d6631d073194?q=80&w=2070')] bg-[length:2000px_cover] bg-[top_-6rem_left_-10rem] p-8 lg:container`}
+      >
+        <div className="mb-[30px] max-w-2xl">
+          <HeroCard
+            actionContent={t('home.card-1-action-content')}
+            backgroundColor="#006DB3F2"
+            content={t('home.card-1-content')}
+            title={t('home.card-1-title')}
+            href={profileIndexLink}
+          />
+        </div>
+        <div className="grid grid-flow-row auto-rows-max grid-cols-3 gap-[32px] lg:container">
+          <HeroCard backgroundColor="#00A8B3F2" title={t('home.card-2-title')} href={profileIndexLink} />
+          <HeroCard backgroundColor="#EE7C45F2" title={t('home.card-3-title')} href={profileIndexLink} />
+          <HeroCard backgroundColor="#CD4EB3F2" title={t('home.card-4-title')} href={profileIndexLink} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable sonarjs/no-duplicate-string */
 import { Route, Routes, NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet-async';
@@ -11,26 +10,12 @@ import './Root.css';
 
 const Root = () => {
   const { t, i18n } = useTranslation();
-
-  const links = [
-    { to: `/${i18n.language}`, text: t('home') },
-    { to: `/${i18n.language}/${t('slugs.profile.index')}`, text: t('profile') },
-    { to: `/${i18n.language}/${t('slugs.about-us')}`, text: t('about-us') },
-    { to: `/${i18n.language}/${t('slugs.about-us')}/${t('slugs.terms-of-service')}`, text: t('terms-of-service') },
-    {
-      to: `/${i18n.language}/${t('slugs.about-us')}/${t('slugs.accessibility-statement')}`,
-      text: t('accessibility-statement'),
-    },
-    { to: `/${i18n.language}/${t('slugs.about-us')}/${t('slugs.privacy-policy')}`, text: t('privacy-policy') },
-    { to: `/${i18n.language}/${t('slugs.about-us')}/${t('slugs.cookies')}`, text: t('cookies') },
-  ];
-
   return (
     <>
       <Helmet>
         <html lang={i18n.language} />
       </Helmet>
-      <header className="sticky top-0">
+      <header className="sticky top-0 z-10">
         <NavigationBar
           user={{
             name: 'John Doe',
@@ -43,27 +28,6 @@ const Root = () => {
             },
           }}
         />
-      </header>
-      <header className="bg-[#f2a93b] p-3">
-        <h1>{t('header')}</h1>
-        <ul className="flex flex-wrap gap-3">
-          {['en', 'fi', 'sv'].map((lng) => (
-            <li key={lng}>
-              <NavLink to={`/${lng}`} className="block p-3">
-                {lng}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-        <ul className="flex flex-wrap gap-3">
-          {links.map((link) => (
-            <li key={link.text}>
-              <NavLink to={link.to} className="block p-3">
-                {link.text}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
       </header>
       <main className="p-3">
         <Routes>
