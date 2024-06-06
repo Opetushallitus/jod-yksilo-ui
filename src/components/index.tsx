56948943c55cf2ae78718001ea6369b28706b580
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-06-06 09:40:13 +0300

    OPHJOD-488: Refactor OsaamisSuosittelija into own reusable component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { Title } from './Title/Title';
 export { MainLayout } from './MainLayout/MainLayout';
+export { OsaamisSuosittelija } from './OsaamisSuosittelija/OsaamisSuosittelija';
 export { SimpleNavigationList } from './MainLayout/SimpleNavigationList';
 export { RoutesNavigationList, type RoutesNavigationListProps } from './MainLayout/RoutesNavigationList';
