7dae88fced6bbba30dd41fdeab7ddd013f8a4b8f
Sauli Anto <sauli.anto@gofore.com>
2024-06-06 13:02:51 +0300

    OPHJOD-495: Include assets from design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss';
 import designSystem from '@jod/design-system/tailwind.config';
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@jod/design-system/lib/**/*.{js,ts,jsx,tsx}'],
   presets: [designSystem],
   plugins: [],
 } satisfies Config;
