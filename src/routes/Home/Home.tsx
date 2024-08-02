96cfc10d8c0acae87916a8a6275dee6b0481f832
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-08-02 13:59:14 +0300

    OPHJOD-549: Update DS and implement HeroCard changes

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,11 +1,19 @@
 import { Title } from '@/components';
 import { HeroCard } from '@jod/design-system';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
 
   const toolIndexLink = `/${i18n.language}/${t('slugs.tool.index')}`;
+  const smHeroCardProps: Partial<React.ComponentProps<typeof HeroCard>> = {
+    href: toolIndexLink,
+    onClick: () => navigate(toolIndexLink),
+    size: 'sm',
+    textColor: '#000',
+  };
 
   return (
     <main role="main" className="mx-auto w-full max-w-screen-lg" id="jod-main">
@@ -13,18 +21,12 @@ const Home = () => {
       <div className="mx-auto bg-[url(@/../assets/hero.jpeg)] bg-[top_-6rem_left_-5rem] py-11">
         <div className="mx-auto flex max-w-[1140px] flex-col gap-11 px-6">
           <div className="mb-[40px] max-w-2xl">
-            <HeroCard
-              actionContent={t('home.card-1-action-content')}
-              backgroundColor="#006DB3F2"
-              content={t('home.card-1-content')}
-              title={t('home.card-1-title')}
-              href={toolIndexLink}
-            />
+            <HeroCard backgroundColor="#006DB3F2" content={t('home.card-1-content')} title={t('home.card-1-title')} />
           </div>
           <div className="grid grid-flow-row auto-rows-max grid-cols-1 gap-[32px] sm:grid-cols-3">
-            <HeroCard backgroundColor="#00A8B3F2" title={t('home.card-2-title')} href={toolIndexLink} />
-            <HeroCard backgroundColor="#EE7C45F2" title={t('home.card-3-title')} href={toolIndexLink} />
-            <HeroCard backgroundColor="#CD4EB3F2" title={t('home.card-4-title')} href={toolIndexLink} />
+            <HeroCard {...smHeroCardProps} backgroundColor="#00A8B3F2" title={t('home.card-2-title')} />
+            <HeroCard {...smHeroCardProps} backgroundColor="#EE7C45F2" title={t('home.card-3-title')} />
+            <HeroCard {...smHeroCardProps} backgroundColor="#CD4EB3F2" title={t('home.card-4-title')} />
           </div>
         </div>
       </div>
