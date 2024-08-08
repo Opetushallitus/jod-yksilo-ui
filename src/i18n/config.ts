097934e657341f02647a5c0270d34ee6f4d6a760
Markus Boman <markus.boman@elenium.fi>
2024-08-08 18:39:12 +0300

    OPHJOD-424: Add language selector

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,21 +1,24 @@
 import i18n, { type Resource } from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import translationEn from './en/translation.json';
 import translationFi from './fi/translation.json';
+import translationSv from './sv/translation.json';
+
+export type LangCode = 'fi' | 'sv' | 'en';
+
+export const supportedLanguageCodes: LangCode[] = ['fi', 'sv', 'en'];
 
 export const resources: Resource = {
-  en: {},
-  fi: {
-    translation: translationFi,
-  },
-  sv: {},
+  en: { translation: translationEn },
+  fi: { translation: translationFi },
+  sv: { translation: translationSv },
 };
-
-export const lng = 'fi';
 
 export const fallbackLng = 'fi';
 
 void i18n.use(initReactI18next).init({
-  lng,
+  lng: 'fi',
+  supportedLngs: supportedLanguageCodes,
   fallbackLng,
   resources,
 });
