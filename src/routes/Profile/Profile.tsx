679da32ed3f17fac36a6de66b13ae8c8356af413
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-08-15 15:42:48 +0300

    OPHJOD-617: Populate MegaMenu with navigation links

--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -1,41 +1,8 @@
-import { useTranslation } from 'react-i18next';
+import { profileRoutes } from '@/routeDefinitions/profileRoutes';
 import { Outlet } from 'react-router-dom';
 
 const Profile = () => {
-  const { t } = useTranslation();
-
-  const routes = [
-    {
-      name: t('profile.preferences'),
-      path: t('slugs.profile.preferences'),
-    },
-    {
-      name: t('profile.favorites'),
-      path: t('slugs.profile.favorites'),
-    },
-    {
-      name: t('profile.competences'),
-      path: t('slugs.profile.competences'),
-    },
-    {
-      name: t('profile.work-history'),
-      path: t('slugs.profile.work-history'),
-    },
-    {
-      name: t('profile.education-history'),
-      path: t('slugs.profile.education-history'),
-    },
-    {
-      name: t('profile.free-time-activities'),
-      path: t('slugs.profile.free-time-activities'),
-    },
-    {
-      name: t('profile.something-else'),
-      path: t('slugs.profile.something-else'),
-    },
-  ];
-
-  return <Outlet context={routes} />;
+  return <Outlet context={profileRoutes} />;
 };
 
 export default Profile;
--- src/routes/UserGuide/UserGuide.tsx
+++ src/routes/UserGuide/UserGuide.tsx
@@ -1,41 +1,16 @@
 import { MainLayout, RoutesNavigationList, SimpleNavigationList } from '@/components';
+import { userGuideRoutes } from '@/routeDefinitions/userGuideRoutes';
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
 
 const UserGuide = () => {
   const { t } = useTranslation();
-  const routes = [
-    {
-      name: t('what-is-the-service'),
-      path: t('slugs.user-guide.what-is-the-service'),
-    },
-    {
-      name: t('who-is-the-service-for'),
-      path: t('slugs.user-guide.who-is-the-service-for'),
-    },
-    {
-      name: t('how-do-i-use-the-service'),
-      path: t('slugs.user-guide.how-do-i-use-the-service'),
-    },
-    {
-      name: t('where-can-i-get-more-help'),
-      path: t('slugs.user-guide.where-can-i-get-more-help'),
-    },
-    {
-      name: t('who-provides-the-service'),
-      path: t('slugs.user-guide.who-provides-the-service'),
-    },
-    {
-      name: t('how-do-i-give-feedback'),
-      path: t('slugs.user-guide.how-do-i-give-feedback'),
-    },
-  ];
 
   return (
     <MainLayout
       navChildren={
         <SimpleNavigationList title={t('user-guide')}>
-          <RoutesNavigationList routes={routes} />
+          <RoutesNavigationList routes={userGuideRoutes} />
         </SimpleNavigationList>
       }
     >
