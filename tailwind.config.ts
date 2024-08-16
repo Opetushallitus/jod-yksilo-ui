4398e5b885232f405e6f34347333cae4fadff343
Sauli Anto <sauli.anto@gofore.com>
2024-08-16 14:37:32 +0300

    OPHJOD-501: Update prefix to Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from 'tailwindcss';
 import designSystem from '@jod/design-system/tailwind.config';
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@jod/design-system/lib/**/*.{js,ts,jsx,tsx}'],
+  prefix: '',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   presets: [designSystem],
   plugins: [],
 } satisfies Config;
