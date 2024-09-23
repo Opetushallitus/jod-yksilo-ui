0f903bcd55e617e2244362d28e089f89787e8cb0
Sauli Anto <sauli.anto@gofore.com>
2024-09-23 12:26:20 +0300

    OPHJOD-791: Refactor Home to Link instead of onClick handler

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,19 +1,10 @@
 import { Title } from '@/components';
 import { HeroCard } from '@jod/design-system';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   const { t, i18n } = useTranslation();
-  const navigate = useNavigate();
-
-  const toolIndexLink = `/${i18n.language}/${t('slugs.tool.index')}`;
-  const smHeroCardProps: Partial<React.ComponentProps<typeof HeroCard>> = {
-    href: toolIndexLink,
-    onClick: () => navigate(toolIndexLink),
-    size: 'sm',
-    textColor: '#000',
-  };
 
   return (
     <main role="main" className="mx-auto w-full max-w-screen-lg" id="jod-main">
@@ -24,9 +15,16 @@ const Home = () => {
             <HeroCard backgroundColor="#006DB3F2" content={t('home.card-1-content')} title={t('home.card-1-title')} />
           </div>
           <div className="grid grid-flow-row auto-rows-max grid-cols-1 gap-[32px] md:grid-cols-3">
-            <HeroCard {...smHeroCardProps} backgroundColor="#00A8B3F2" title={t('home.card-2-title')} />
-            <HeroCard {...smHeroCardProps} backgroundColor="#EE7C45F2" title={t('home.card-3-title')} />
-            <HeroCard {...smHeroCardProps} backgroundColor="#CD4EB3F2" title={t('home.card-4-title')} />
+            <HeroCard
+              to={`/${i18n.language}/${t('slugs.tool.index')}/${t('slugs.tool.competences')}`}
+              linkComponent={Link}
+              size="sm"
+              textColor="#000"
+              backgroundColor="#00A8B3F2"
+              title={t('home.card-2-title')}
+            />
+            <HeroCard size="sm" textColor="#000" backgroundColor="#EE7C45F2" title={t('home.card-3-title')} />
+            <HeroCard size="sm" textColor="#000" backgroundColor="#CD4EB3F2" title={t('home.card-4-title')} />
           </div>
         </div>
       </div>
