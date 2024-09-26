bc43ea7f633792518df3748fca2ffa0cf52a7997
Sauli Anto <sauli.anto@gofore.com>
2024-09-26 08:34:29 +0300

    OPHJOD-803: Refactor home route to adjust hero layout

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -9,12 +9,12 @@ const Home = () => {
   return (
     <main role="main" className="mx-auto w-full max-w-screen-lg" id="jod-main">
       <Title value={t('home.title')} />
-      <div className="mx-auto bg-[url(@/../assets/hero.jpeg)] bg-[top_-6rem_left_-5rem] py-11">
-        <div className="mx-auto flex max-w-[1140px] flex-col gap-11 px-6 hyphens-auto lg:hyphens-none">
-          <div className="mb-[40px] max-w-2xl">
+      <div className="mx-auto bg-[url(@/../assets/hero.jpeg)] bg-[top_-6rem_left_-5rem] py-5 sm:py-11">
+        <div className="mx-auto flex max-w-[1140px] flex-col gap-3 sm:gap-11 px-5 sm:px-6 hyphens-auto lg:hyphens-none">
+          <div className="sm:mb-[40px] max-w-2xl">
             <HeroCard backgroundColor="#006DB3F2" content={t('home.card-1-content')} title={t('home.card-1-title')} />
           </div>
-          <div className="grid grid-flow-row auto-rows-max grid-cols-1 gap-[32px] md:grid-cols-3">
+          <div className="grid grid-flow-row auto-rows-max grid-cols-1 gap-3 sm:gap-7 md:grid-cols-3">
             <HeroCard
               to={`/${i18n.language}/${t('slugs.tool.index')}/${t('slugs.tool.competences')}`}
               linkComponent={Link}
