1a7e3a9a0937c137f6b1f757a96dcc63e0dd7ce2
Sauli Anto <sauli.anto@gofore.com>
2024-10-01 11:36:41 +0300

    OPHJOD-806: Refactor home route in mobile

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,11 +1,15 @@
 import { components } from '@/api/schema';
 import { Title } from '@/components';
-import { HeroCard } from '@jod/design-system';
+import { HeroCard, useMediaQueries } from '@jod/design-system';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useRouteLoaderData } from 'react-router-dom';
 
-const Home = () => {
+interface CardsProps {
+  className?: string;
+}
+
+const Cards = ({ className }: CardsProps) => {
   const {
     t,
     i18n: { language },
@@ -42,41 +46,53 @@ const Home = () => {
   }, [data, language, t]);
 
   return (
-    <main role="main" className="mx-auto w-full max-w-screen-lg" id="jod-main">
-      <Title value={t('home.title')} />
-      <div className="mx-auto bg-[url(@/../assets/hero.jpeg)] bg-[top_-6rem_left_-5rem] py-5 sm:py-11">
-        <div className="mx-auto flex max-w-[1140px] flex-col gap-3 sm:gap-11 px-5 sm:px-6 hyphens-auto lg:hyphens-none">
-          <div className="sm:mb-[40px] max-w-2xl">
-            <HeroCard backgroundColor="#006DB3F2" content={t('home.card-1-content')} title={t('home.card-1-title')} />
-          </div>
-          <div className="grid grid-flow-row auto-rows-max grid-cols-1 gap-3 sm:gap-7 md:grid-cols-3">
-            <HeroCard
-              to={`/${language}/${t('slugs.tool.index')}/${t('slugs.tool.competences')}`}
-              linkComponent={Link}
-              size="sm"
-              textColor="#000"
-              backgroundColor="#00A8B3F2"
-              title={t('home.card-2-title')}
-            />
-            <HeroCard
-              to={`${t('slugs.user-guide.index')}/${t('slugs.user-guide.what-is-the-service')}`}
-              linkComponent={Link}
-              size="sm"
-              textColor="#000"
-              backgroundColor="#EE7C45F2"
-              title={t('home.card-3-title')}
-            />
-            <HeroCard
-              to={preferencesLink.to}
-              linkComponent={preferencesLink.component}
-              size="sm"
-              textColor="#000"
-              backgroundColor="#CD4EB3F2"
-              title={t('home.card-4-title')}
-            />
-          </div>
-        </div>
+    <div
+      className={`mx-auto flex max-w-[1140px] flex-col gap-3 sm:gap-11 px-5 sm:px-6 hyphens-auto lg:hyphens-none ${className}`.trim()}
+    >
+      <div className="sm:mb-[40px] max-w-2xl">
+        <HeroCard backgroundColor="#006DB3F2" content={t('home.card-1-content')} title={t('home.card-1-title')} />
       </div>
+      <div className="grid grid-flow-row auto-rows-max grid-cols-1 gap-3 sm:gap-7 md:grid-cols-3">
+        <HeroCard
+          to={`/${language}/${t('slugs.tool.index')}/${t('slugs.tool.competences')}`}
+          linkComponent={Link}
+          size="sm"
+          textColor="#000"
+          backgroundColor="#00A8B3F2"
+          title={t('home.card-2-title')}
+        />
+        <HeroCard
+          to={`${t('slugs.user-guide.index')}/${t('slugs.user-guide.what-is-the-service')}`}
+          linkComponent={Link}
+          size="sm"
+          textColor="#000"
+          backgroundColor="#EE7C45F2"
+          title={t('home.card-3-title')}
+        />
+        <HeroCard
+          to={preferencesLink.to}
+          linkComponent={preferencesLink.component}
+          size="sm"
+          textColor="#000"
+          backgroundColor="#CD4EB3F2"
+          title={t('home.card-4-title')}
+        />
+      </div>
+    </div>
+  );
+};
+
+const Home = () => {
+  const { t } = useTranslation();
+  const { sm } = useMediaQueries();
+
+  return (
+    <main role="main" className="mx-auto w-full max-w-screen-lg bg-white" id="jod-main">
+      <Title value={t('home.title')} />
+      <div className="h-[320px] sm:h-auto mx-auto bg-[url(@/../assets/hero.jpeg)] bg-[length:auto_680px] bg-[top_-2rem_right_-22rem] sm:bg-[length:auto_auto] sm:bg-[top_-6rem_left_-5rem] py-5 sm:py-11">
+        {sm && <Cards />}
+      </div>
+      {!sm && <Cards className="relative -top-11" />}
     </main>
   );
 };
