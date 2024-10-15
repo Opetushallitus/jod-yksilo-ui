44bba03f4936965b13deb6210dc408f1da1c2aa8
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-10-15 12:53:46 +0300

    OPHJOD-888: Fix wrong api path in resetTool middleware

--- src/api/middlewares/resetTool.ts
+++ src/api/middlewares/resetTool.ts
@@ -2,7 +2,7 @@ import { useToolStore } from '@/stores/useToolStore';
 import { Middleware } from 'openapi-fetch';
 
 // API paths that should not reset the tool state
-const ignoredPaths = ['/api/profiili/suosikki'];
+const ignoredPaths = ['/api/profiili/suosikit'];
 
 export const resetTool: Middleware = {
   onRequest(request) {
