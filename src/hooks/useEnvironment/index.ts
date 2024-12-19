e8d8cc16258b605c411b97b0f07edd7f51a4a551
Sami Korpela <sami.korpela@gofore.com>
2024-12-19 14:40:10 +0200

    OPHJOD-1162: Update logic to recognize dev-environment

--- src/hooks/useEnvironment/index.ts
+++ src/hooks/useEnvironment/index.ts
@@ -2,7 +2,7 @@ export const useEnvironment = () => {
   const hostname = window.location.hostname;
 
   return {
-    isDev: ['localhost', 'jodkehitys'].some((str) => hostname.includes(str)),
+    isDev: import.meta.env.DEV || ['localhost', 'jodkehitys'].some((str) => hostname.includes(str)),
     isTst: hostname.includes('jodtestaus'),
     isPrd: !['localhost', 'jodkehitys', 'jodtestaus'].some((str) => hostname.includes(str)),
   };
