acae815ed047c70247cf5f5299f527d87a2de4d1
Nhut Do <nhut.do@elenium.fi>
2025-02-28 16:47:51 +0200

    OPHJOD-1300: Suggested code review changes

--- src/routes/Tool/Interests.tsx
+++ src/routes/Tool/Interests.tsx
@@ -126,7 +126,7 @@ const Interests = () => {
                 onClick={() => {
                   toolStore.setVirtualAssistantOpen(true);
                   window.scrollTo({ top: 0, behavior: 'smooth' });
-                  setTimeout(() => document.getElementById('kiinnostuksetView')?.focus(), 0);
+                  setTimeout(() => document.getElementById('kiinnostuksetTitle')?.focus(), 0);
                 }}
                 type="button"
                 className="cursor-pointer"
