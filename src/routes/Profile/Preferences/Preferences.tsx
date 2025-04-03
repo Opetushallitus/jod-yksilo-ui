318613a1938b3b1746d008dfee18919bbe678b46
Sauli Anto <sauli.anto@gofore.com>
2025-04-03 12:30:25 +0300

    OPHJOD-1461: Move profile deletion to preferences

--- src/routes/Profile/Preferences/Preferences.tsx
+++ src/routes/Profile/Preferences/Preferences.tsx
@@ -1,4 +1,6 @@
+import { client } from '@/api/client';
 import { MainLayout } from '@/components';
+import { Button, ConfirmDialog } from '@jod/design-system';
 import { useTranslation } from 'react-i18next';
 import { ProfileNavigationList } from '../components';
 
@@ -6,23 +8,35 @@ const Preferences = () => {
   const { t } = useTranslation();
   const title = t('profile.preferences.title');
 
+  const deleteProfile = async () => {
+    if (await client.DELETE('/api/profiili/yksilo')) {
+      window.location.href = '/';
+    }
+  };
+
   return (
     <MainLayout navChildren={<ProfileNavigationList />}>
       <title>{title}</title>
-      <h1 className="mb-5 text-heading-2 sm:text-heading-1">{t('profile.preferences.title')}</h1>
-
+      <h1 className="mb-5 text-heading-2 sm:text-heading-1">{t('preferences.title')}</h1>
       <div className="mb-8 text-body-md flex flex-col gap-7">
-        <p className="bg-todo">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet at tortor sed rutrum. Maecenas facilisis
-          pretium velit, in consectetur ipsum scelerisque quis. Curabitur eu diam a massa pretium commodo. Pellentesque
-          habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse eleifend lorem
-          nunc, eu vehicula orci malesuada vitae. Nam ac arcu finibus, feugiat ipsum et, bibendum augue. Curabitur orci
-          risus, tempus in mollis aliquam, tempor eu nisl. Etiam tellus nibh, auctor lobortis consectetur in, commodo
-          vel quam. Suspendisse ac libero porttitor, eleifend elit a, interdum sem. In congue nunc non rhoncus
-          imperdiet. Etiam magna neque, mollis et congue nec, ullamcorper ut tellus. Donec ac luctus quam. Etiam mattis
-          magna eu augue efficitur tristique.
-        </p>
+        <p className="bg-todo">{t('preferences.description')}</p>
       </div>
+      <section>
+        <h2 className="text-heading-2-mobile sm:text-heading-2 mb-3">{t('preferences.delete-profile.title')}</h2>
+        <p className="text-body-md mb-5">{t('preferences.delete-profile.description')}</p>
+        <ConfirmDialog
+          title={t('preferences.delete-profile.action')}
+          onConfirm={() => void deleteProfile()}
+          confirmText={t('delete')}
+          cancelText={t('cancel')}
+          variant="destructive"
+          description={t('preferences.delete-profile.confirm')}
+        >
+          {(showDialog: () => void) => (
+            <Button variant="white-delete" label={t('preferences.delete-profile.action')} onClick={showDialog} />
+          )}
+        </ConfirmDialog>
+      </section>
     </MainLayout>
   );
 };
--- src/routes/Profile/ProfileFront/ProfileFront.tsx
+++ src/routes/Profile/ProfileFront/ProfileFront.tsx
@@ -1,9 +1,5 @@
-import { client } from '@/api/client';
 import { components } from '@/api/schema';
 import { MainLayout } from '@/components';
-import { useActionBar } from '@/hooks/useActionBar';
-import { Button, ConfirmDialog } from '@jod/design-system';
-import { createPortal } from 'react-dom';
 import { useTranslation } from 'react-i18next';
 import { useRouteLoaderData } from 'react-router';
 import { ProfileNavigationList } from '../components';
@@ -11,20 +7,7 @@ import { ProfileNavigationList } from '../components';
 const ProfileFront = () => {
   const { t } = useTranslation();
   const title = t('profile.front.title');
-
-  const actionBar = useActionBar();
   const rootLoaderData = useRouteLoaderData('root') as components['schemas']['YksiloCsrfDto'];
-
-  const deleteAccount = async () => {
-    try {
-      const ok = await client.DELETE('/api/profiili/yksilo');
-      if (ok) {
-        window.location.href = '/';
-      }
-    } catch (_e) {
-      // Do nothing
-    }
-  };
 
   return (
     <MainLayout navChildren={<ProfileNavigationList />}>
@@ -48,25 +31,6 @@ const ProfileFront = () => {
         <p>{t('profile.preferences.paragraph-3')}</p>
         <p>{t('profile.preferences.paragraph-4')}</p>
       </div>
-      {actionBar &&
-        createPortal(
-          <div className="mx-auto flex max-w-[1140px] flex-wrap gap-4 px-5 py-4 sm:gap-5 sm:px-6 sm:py-5">
-            <Button variant="white" label={`TODO: ${t('preferences.share-my-competences')}`} />
-            <ConfirmDialog
-              title={t('preferences.delete-my-account')}
-              onConfirm={() => void deleteAccount()}
-              confirmText={t('delete')}
-              cancelText={t('cancel')}
-              variant="destructive"
-              description={t('preferences.confirm-delete-my-account')}
-            >
-              {(showDialog: () => void) => (
-                <Button variant="white-delete" label={t('preferences.delete-my-account')} onClick={showDialog} />
-              )}
-            </ConfirmDialog>
-          </div>,
-          actionBar,
-        )}
     </MainLayout>
   );
 };
