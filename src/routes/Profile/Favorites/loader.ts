3b364a34ba438aecf7c68186aabe644106e51811
Sauli Anto <sauli.anto@gofore.com>
2025-04-24 15:33:02 +0300

    OPHJOD-1530: Fix favorites loader

--- src/routes/Profile/Favorites/loader.ts
+++ src/routes/Profile/Favorites/loader.ts
@@ -1,10 +1,14 @@
+import { client } from '@/api/client';
 import { components } from '@/api/schema';
 import { DEFAULT_PAGE_SIZE } from '@/constants';
 import { useSuosikitStore } from '@/stores/useSuosikitStore';
 import { LoaderFunction } from 'react-router';
 
-export default (async () => {
-  const { fetchSuosikit, fetchPage } = useSuosikitStore.getState();
+export default (async ({ request }) => {
+  const { setExcludedIds, fetchSuosikit, fetchPage } = useSuosikitStore.getState();
+  const response = await client.GET('/api/profiili/paamaarat', { signal: request.signal });
+  const paamaarat = response.data ?? [];
+  setExcludedIds(paamaarat.map((pm) => pm.mahdollisuusId));
   await fetchSuosikit();
   await fetchPage({ page: 1, pageSize: DEFAULT_PAGE_SIZE });
   return null;
