b988fb484cda5a65451929b34ac7ec203912c7f0
Sami Korpela <sami.korpela@gofore.com>
2025-05-22 15:09:08 +0300

    OPHJOD-1617: Update to use MoreInfo and updated HeroCard components

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,58 +1,16 @@
 import { components } from '@/api/schema';
-import { Button, tidyClasses as tc } from '@jod/design-system';
+import { Button, HeroCard, MoreInfo, tidyClasses as tc } from '@jod/design-system';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { MdArrowForward } from 'react-icons/md';
 import { Link, useRouteLoaderData } from 'react-router';
 import { generateProfileLink } from '../Profile/utils';
 
-const InfoLink = ({ to, label, lang = 'fi' }: { to: string; label: string; lang: string }) => (
-  <Link to={`/${lang}/${to}`} className="flex justify-center gap-4 text-body-md font-semibold">
-    {label} <MdArrowForward size={24} />
-  </Link>
-);
-
 const LinkComponent = (to: string) => {
   const Component = ({ children }: { children: React.ReactNode }) => <Link to={to}>{children}</Link>;
   Component.displayName = 'LinkComponent';
   return Component;
 };
-
-interface CardProps {
-  buttonLabel?: string;
-  buttonLink?: string;
-  buttonVariant?: React.ComponentProps<typeof Button>['variant'];
-  className?: string;
-  content: string;
-  headingClassName?: string;
-  title: string;
-}
-const Card = ({
-  buttonLabel,
-  buttonLink,
-  buttonVariant = 'white',
-  className = 'bg-accent text-white',
-  content,
-  headingClassName = 'text-heading-2',
-  title,
-}: CardProps) => (
-  <div className={tc(`flex flex-col gap-5 rounded-lg p-6 ${className}`)}>
-    <div className={headingClassName}>{title}</div>
-    <div className="text-body-lg">{content}</div>
-    {buttonLabel && buttonLink && (
-      <div className="mt-auto">
-        <Button
-          className="mt-4"
-          icon={<MdArrowForward size={24} />}
-          iconSide="right"
-          label={buttonLabel}
-          variant={buttonVariant}
-          LinkComponent={LinkComponent(buttonLink)}
-        />
-      </div>
-    )}
-  </div>
-);
 
 const FeatureBox = ({ title }: { title: string }) => {
   const { t } = useTranslation();
@@ -69,14 +27,14 @@ interface ContainerProps {
 }
 
 const FullWidthContainer = ({ className = '', children }: ContainerProps) => (
-  <div className={tc(['bg-no-repeat', 'bg-[length:1440px_auto]', 'flex', 'justify-start', className])}>
-    <div className="w-[1092px] mx-auto p-5">{children}</div>
+  <div className={tc(['bg-no-repeat', 'bg-[length:1440px_auto]', 'flex', 'justify-start', 'py-8', className])}>
+    <div className="w-[1092px] mx-auto px-5 sm:px-6">{children}</div>
   </div>
 );
 const Content = ({ className = '', title, children }: ContainerProps & { title?: string }) => {
   const { t } = useTranslation();
   return (
-    <div className={tc(['mx-auto', 'max-w-[1092px]', 'py-7', 'px-5', 'flex', 'flex-col', 'gap-7', className])}>
+    <div className={tc(['mx-auto', 'max-w-[1092px]', 'py-7', 'px-5 sm:px-6', 'flex', 'flex-col', 'gap-7', className])}>
       {title && <h2 className="text-heading-1">{t(`home.${title}`)}</h2>}
       {children}
     </div>
@@ -98,6 +56,29 @@ const Home = () => {
   const toolLink = `/${language}/${t('slugs.tool.index')}/${t('slugs.tool.competences')}`;
   const placeholderLink = location.href;
 
+  const moreInfoLinks = [
+    {
+      to: `${t('slugs.user-guide.index')}/${t('slugs.about-us')}`,
+      label: t('about-us'),
+    },
+    {
+      to: `${infoSlug}/${t('slugs.privacy-policy')}`,
+      label: t('privacy-policy-and-cookies'),
+    },
+    {
+      to: `${infoSlug}/${t('slugs.data-sources')}`,
+      label: t('data-sources'),
+    },
+    {
+      to: `${infoSlug}/${t('slugs.about-ai')}`,
+      label: t('about-ai'),
+    },
+    {
+      to: `${infoSlug}/${t('slugs.accessibility-statement')}`,
+      label: t('accessibility-statement'),
+    },
+  ];
+
   return (
     <main role="main" className="mx-auto w-full max-w-(--breakpoint-xl) bg-white" id="jod-main">
       <title>{t('osaamispolku')}</title>
@@ -111,24 +92,26 @@ const Home = () => {
         ])}
       >
         <div className="max-w-2xl">
-          <Card title={t('home.hero-title')} content={t('home.hero-content')} headingClassName="text-hero" />
+          <HeroCard title={t('home.hero-title')} content={t('home.hero-content')} />
         </div>
       </FullWidthContainer>
 
       <Content className="flex sm:flex-row gap-8 justify-evenly flex-col">
-        <Card
+        <HeroCard
+          size="sm"
           title={t('home.card-1-title')}
           content={t('home.card-1-content')}
           buttonLabel={t('home.create-own-profile')}
-          className="bg-accent text-white flex-1"
-          buttonLink={preferencesLink.to}
+          to={preferencesLink.to}
+          LinkComponent={Link}
         />
-        <Card
+        <HeroCard
+          size="sm"
           title={t('home.card-2-title')}
           content={t('home.card-2-content')}
           buttonLabel={t('home.explore-opportunities')}
-          className="bg-accent text-white flex-1"
-          buttonLink={toolLink}
+          to={toolLink}
+          LinkComponent={Link}
         />
       </Content>
 
@@ -163,14 +146,16 @@ const Home = () => {
         </div>
       </Content>
 
-      <FullWidthContainer className="h-[360px] sm:h-[306px] bg-[url(@/../assets/home-1.avif)] bg-[center_-325px] items-center">
+      <FullWidthContainer className="bg-[url(@/../assets/home-1.avif)] bg-[center_-325px] items-center">
         <div className="max-w-2xl">
-          <Card
+          <HeroCard
+            size="sm"
             content={t('home.need-personal-guidance-content')}
             title={t('home.need-personal-guidance')}
             buttonLabel={t('home.go-to-service-directory')}
-            className="bg-secondary-2 text-white"
-            buttonLink={placeholderLink}
+            to={placeholderLink}
+            backgroundColor="#00A8B3"
+            LinkComponent={Link}
           />
         </div>
       </FullWidthContainer>
@@ -180,54 +165,28 @@ const Home = () => {
         <div className="h-[200px] bg-bg-gray-2" />
       </Content>
 
-      <FullWidthContainer className="h-[360px] sm:h-[306px] bg-[url(@/../assets/home-1.avif)] items-center bg-[center_-340px]">
+      <FullWidthContainer className="bg-[url(@/../assets/home-1.avif)] items-center bg-[center_-340px]">
         <div className="max-w-2xl">
-          <Card
+          <HeroCard
+            size="sm"
             content={t('home.tell-us-what-you-think-content')}
             title={t('home.tell-us-what-you-think')}
             buttonLabel={t('home.provide-feedback')}
-            className="bg-[#333] text-white"
             buttonVariant="white"
-            buttonLink={placeholderLink}
+            backgroundColor="#333"
+            to={placeholderLink}
+            LinkComponent={Link}
           />
         </div>
       </FullWidthContainer>
 
-      <FullWidthContainer className="text-white bg-[#333] p-8">
-        <div className="flex flex-col gap-3">
-          <div className="text-heading-2">{t('home.want-to-know-more')}</div>
-          <p className="text-body-sm">{t('home.want-to-know-more-content')}</p>
-          <ul className="flex flex-col gap-3 justify-start items-start">
-            <li>
-              <InfoLink
-                to={`${t('slugs.slugs.user-guide.index')}/${t('slugs.about-us')}`}
-                label={t('about-us')}
-                lang={language}
-              />
-            </li>
-            <li>
-              <InfoLink
-                to={`${infoSlug}/${t('slugs.privacy-policy')}`}
-                label={t('privacy-policy-and-cookies')}
-                lang={language}
-              />
-            </li>
-            <li>
-              <InfoLink to={`${infoSlug}/${t('slugs.data-sources')}`} label={t('data-sources')} lang={language} />
-            </li>
-            <li>
-              <InfoLink to={`${infoSlug}/${t('slugs.about-ai')}`} label={t('about-ai')} lang={language} />
-            </li>
-            <li>
-              <InfoLink
-                to={`${infoSlug}/${t('slugs.accessibility-statement')}`}
-                label={t('accessibility-statement')}
-                lang={language}
-              />
-            </li>
-          </ul>
-        </div>
-      </FullWidthContainer>
+      <MoreInfo
+        title={t('home.want-to-know-more')}
+        description={t('home.want-to-know-more-content')}
+        language={language}
+        links={moreInfoLinks}
+        LinkComponent={Link}
+      />
     </main>
   );
 };
