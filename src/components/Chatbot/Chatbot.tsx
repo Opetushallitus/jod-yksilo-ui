02b9585e2dac4b0d97ddb87cb1c5298144fc6e8b
Sauli Anto <sauli.anto@gofore.com>
2025-06-30 15:27:43 +0300

    OPHJOD-1756: Refactor chatbot styles and translations

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable sonarjs/todo-tag */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { loadAiAgentFloat } from './loadAiAgentFloat';
 
-// TODO: Use environment and language specific agent IDs
 const agents: Record<'fi' | 'sv' | 'en', string> = {
   fi: '2c134474-326f-4456-9139-8e585a569a9a',
   sv: 'd41ea75b-628f-4420-9e4a-7431ffabb047',
@@ -22,35 +21,97 @@ export const Chatbot = () => {
   }, []);
 
   return (
-    <>
-      {/* @ts-ignore */}
-      <ai-agent-float
+    <ai-agent-styles
+      primary="#ee7c45"
+      primarycontrast="#00464a"
+      error="#de342b"
+      fontfamily="Arial"
+      paper="#ffffff"
+      text="#1f1f1f"
+    >
+      <ai-agent-float-container
         key={language} // Force re-render on language change
-        agent={agents[language as 'fi' | 'sv' | 'en']}
-        server="https://okm-ps32.aiagent.fi/services/sva"
-        agentname={t('chatbot.agent-name')}
-        primary="#20c997"
-        primarycontrast="#eeedeb"
-        paper="#eeedeb"
-        banner="https://aiw-dev.aiagent.fi/integrations/assets/twoday-wordmark-RGB_WHITE-Df4N7RQF.png"
-        agenticon="https://aiw-dev.aiagent.fi/integrations/assets/twoday-logomark-RGB-WHITE-C2C4UZdK.png"
-        header={t('chatbot.header')}
-        errormessage={t('chatbot.error-message')}
-        error="#ED1C24"
-        text="#000000"
-        multilineinput="true"
-        openwindowtext={t('chatbot.open-window-text')}
-        greeting={t('chatbot.greeting')}
-        textinputplaceholder={t('chatbot.text-input-placeholder')}
-        textinputhelper={t('chatbot.text-input-helper')}
-        fontfamily="sans-serif"
-        openchaticon="https://aiw-dev.aiagent.fi/integrations/assets/twoday-logomark-RGB-WHITE-C2C4UZdK.png"
-        feedbacktype="Basic"
         width="30em"
-        dense="false"
         enablescroll="true"
-        zindex="1"
-      />
-    </>
+        zindex="1000"
+        background="primary"
+        backgroundpaper="true"
+        language={language}
+        agenticon={`${import.meta.env.BASE_URL}chatbot-icon.svg`}
+        openwindowtext={t('chatbot.open-window-text')}
+        openchatsize="large"
+      >
+        <div slot="header" className="flex flex-row items-center justify-between grow gap-3 p-5">
+          <ai-agent-text>{t('chatbot.header')}</ai-agent-text>
+          <ai-agent-icon-button
+            icon="menu"
+            onClick={() => {
+              window.dispatchEvent(
+                new CustomEvent('ai-agent-view-switch-change-view', {
+                  detail: { view: 1 },
+                }),
+              );
+            }}
+          />
+        </div>
+        <ai-agent-view-switch slot="content" initialview="0">
+          <ai-agent-embed
+            slot="view-0"
+            agent={agents[language as 'fi' | 'sv' | 'en']}
+            server="https://okm-ps32.aiagent.fi/services/sva"
+            language={language}
+            paper="#ffffff"
+            primary="#ee7c45"
+            primarycontrast="#00464a"
+            text="#1f1f1f"
+            error="#de342b"
+            fontfamily="Arial"
+            agentname={t('chatbot.agent-name')}
+            dense="false"
+            errormessage={t('chatbot.error-message')}
+            feedbacktype="None"
+            greeting={t('chatbot.greeting')}
+            omitgreeting="false"
+            multilineinput="true"
+            textinputplaceholder={t('chatbot.text-input-placeholder')}
+            textinputhelper={t('chatbot.text-input-helper')}
+            width="21.5rem"
+          />
+          <div slot="view-1" className="flex flex-col items-center gap-5">
+            <ai-agent-button
+              onClick={() => {
+                window.dispatchEvent(new CustomEvent('ai-agent-reset-chat'));
+                window.dispatchEvent(
+                  new CustomEvent('ai-agent-view-switch-change-view', {
+                    detail: { view: 0 },
+                  }),
+                );
+              }}
+            >
+              {t('chatbot.erase-chat-history')}
+            </ai-agent-button>
+            <ai-agent-button
+              onClick={() => {
+                window.dispatchEvent(new CustomEvent('ai-agent-save-chat'));
+              }}
+            >
+              {t('chatbot.save-chat-as-csv')}
+            </ai-agent-button>
+            <ai-agent-button
+              variant="outlined"
+              onClick={() => {
+                window.dispatchEvent(
+                  new CustomEvent('ai-agent-view-switch-change-view', {
+                    detail: { view: 0 },
+                  }),
+                );
+              }}
+            >
+              {t('chatbot.close')}
+            </ai-agent-button>
+          </div>
+        </ai-agent-view-switch>
+      </ai-agent-float-container>
+    </ai-agent-styles>
   );
 };
