d393358a5401745985f250073f86078071551dff
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-07-25 15:00:46 +0300

    OPHJOD-1783: Update toaster position

--- src/components/Toaster/Toaster.tsx
+++ src/components/Toaster/Toaster.tsx
@@ -4,14 +4,18 @@ import { Toast as ReactHotToast, useToaster } from 'react-hot-toast/headless';
 type SafeToast = Omit<ReactHotToast, 'message'> & { message: string };
 
 export const Toaster = () => {
-  const { toasts, handlers } = useToaster();
+  const { toasts, handlers } = useToaster({ duration: 4000 });
   const { startPause, endPause } = handlers;
   const safeToasts = toasts.filter((toast): toast is SafeToast => typeof toast.message === 'string');
+  // Header 48px + 16px padding = 64px
+  // Service bar 40px when open, 4px when closed (scrollY > 0)
+  // Margin to edges is 32px according to design
+  const topClass = window.scrollY > 0 ? 'top-[100px]' : 'top-[136px]';
 
   return (
     <div
       role="alert"
-      className="fixed top-[80px] right-4 z-60 flex flex-col gap-4"
+      className={`fixed ${topClass} right-7 z-60 flex flex-col gap-4`}
       onMouseEnter={startPause}
       onMouseLeave={endPause}
     >
