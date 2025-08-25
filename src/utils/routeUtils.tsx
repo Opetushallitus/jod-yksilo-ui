49cb76af294db695b8963307fe52e87ce368a07f
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-08-25 15:58:26 +0300

    OPHJOD-1895: Update opportunity pages to match latest design

--- src/utils/routeUtils.tsx
+++ src/utils/routeUtils.tsx
@@ -95,15 +95,23 @@ export const generateMenuItems = (opts: GenerateMenuItemsOptions): MenuItem[] =>
   return menuItems;
 };
 
+export interface LinkToOpts {
+  useAnchor?: boolean;
+  target?: string;
+}
 /**
  * A function for creating DS style link components, to avoid creating components during runtime.
  * @param to Same as react-routers Link 'to' prop, can be a string or an object.
+ * @param opts Options for the link component
  * @returns A Link component
  */
-export const getLinkTo = (to: React.ComponentProps<typeof Link>['to'], useAnchor = false): LinkComponent => {
+export const getLinkTo = (
+  to: React.ComponentProps<typeof Link>['to'],
+  opts: LinkToOpts = { useAnchor: false, target: '_self' },
+): LinkComponent => {
   const LinkToComponent = ({ children, className }: LinkComponent) =>
-    useAnchor ? (
-      <a className={className} href={typeof to === 'string' ? to : to.pathname}>
+    opts.useAnchor ? (
+      <a className={className} href={typeof to === 'string' ? to : to.pathname} target={opts.target} rel="noreferrer">
         {children}
       </a>
     ) : (
