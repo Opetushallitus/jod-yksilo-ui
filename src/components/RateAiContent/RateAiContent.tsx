b5b903306bd54f3aa2ad5d336d5fcf8446316492
Sami Korpela <sami.korpela@gofore.com>
2025-09-24 13:06:37 +0300

    OPHJOD-2222: Update RateAiContent modal, fix a11y issues, show only on dev

--- src/components/RateAiContent/RateAiContent.tsx
+++ src/components/RateAiContent/RateAiContent.tsx
@@ -1,7 +1,9 @@
+import { useEnvironment } from '@/hooks/useEnvironment';
 import { useModal } from '@/hooks/useModal';
 import { Textarea } from '@jod/design-system';
 import { JodAi, JodThumbDown, JodThumbDownFilled, JodThumbUp, JodThumbUpFilled } from '@jod/design-system/icons';
 import React from 'react';
+import toast from 'react-hot-toast/headless';
 import { useTranslation } from 'react-i18next';
 
 interface RateAiContentProps {
@@ -18,49 +20,68 @@ export const RateAiContent = ({ isLiked, isDisliked, onLike, onDislike, variant 
   const DislikeIcon = isDisliked ? JodThumbDownFilled : JodThumbDown;
   const { showDialog } = useModal();
   const dislikeRef = React.useRef('');
+  const { isDev } = useEnvironment();
 
   const headerText =
     variant === 'kohtaanto' ? t('rate-ai-content.kohtaanto.header') : t('rate-ai-content.mahdollisuus.header');
   const bodyDescription =
     variant === 'kohtaanto' ? t('rate-ai-content.kohtaanto.body') : t('rate-ai-content.mahdollisuus.body');
 
-  return (
+  return isDev ? (
     <div className="bg-accent flex flex-col rounded-lg min-h-[271px] p-6">
       <div className="flex items-start mb-2">
         <div className="text-heading-2 text-white mr-2">{headerText}</div>
         <div className="text-white">
-          <JodAi size={32} />
+          <JodAi aria-label={t('rate-ai-content.icon')} size={32} />
         </div>
       </div>
       <div className="flex items-center mb-2">
         <p className="text-body-lg text-white">{bodyDescription}</p>
       </div>
-      <div className="bg-white flex items-center justify-between px-5 w-[128px] h-9 rounded-[30px] mt-auto">
-        <span className="bg-white rounded-full flex items-center px-3">
-          <LikeIcon className="text-accent cursor-pointer" onClick={onLike} />
-        </span>
-        <div className="h-9 w-1 bg-border-gray" aria-hidden="true" />
-        <span className="bg-white rounded-full flex items-center justify-center px-3">
-          <DislikeIcon
+      <div className="flex flex-row items-center justify-between w-[128px] h-9 rounded-[30px] mt-auto">
+        <button
+          className="bg-white rounded-l-[30px] flex-1 h-full w-full items-center justify-center pl-6 pr-5 flex"
+          aria-label={t('rate-ai-content.like')}
+        >
+          <LikeIcon
             className="text-accent cursor-pointer"
-            onClick={() =>
-              showDialog({
-                title: t('rate-ai-content.modal-header'),
-                content: (
-                  <Textarea
-                    onChange={(e) => {
-                      dislikeRef.current = e.target.value;
-                    }}
-                    placeholder={t('rate-ai-content.modal-placeholder')}
-                  />
-                ),
-                description: t('rate-ai-content.modal-body'),
-                onConfirm: () => onDislike(dislikeRef.current),
-              })
-            }
+            onClick={() => {
+              onLike();
+              toast.success(t('rate-ai-content.toast'));
+            }}
           />
-        </span>
+        </button>
+        <div className="h-9 min-w-1 bg-border-gray" aria-hidden="true" />
+        <button
+          className="bg-white rounded-r-[30px] flex-1 h-full items-center justify-center pl-5 pr-6 flex"
+          aria-label={t('rate-ai-content.dislike')}
+          onClick={() =>
+            showDialog({
+              variant: 'normal',
+              title: t('rate-ai-content.modal.header'),
+              cancelText: t('rate-ai-content.modal.close'),
+              confirmText: t('rate-ai-content.modal.send'),
+              content: (
+                <Textarea
+                  onChange={(e) => {
+                    dislikeRef.current = e.target.value;
+                  }}
+                  placeholder={t('rate-ai-content.modal.placeholder')}
+                />
+              ),
+              description: t('rate-ai-content.modal.body'),
+              onConfirm: () => {
+                onDislike(dislikeRef.current);
+                toast.success(t('rate-ai-content.toast'));
+              },
+            })
+          }
+        >
+          <DislikeIcon className="text-accent cursor-pointer" />
+        </button>
       </div>
     </div>
+  ) : (
+    <></>
   );
 };
