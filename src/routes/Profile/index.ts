6eb98f24ee5111e406fd4ecdc2b0d10f1e805155
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-09-25 15:56:59 +0300

    OPHJOD-1979: Rename landing page to login page and update it to match latest design

--- src/routes/Profile/index.ts
+++ src/routes/Profile/index.ts
@@ -1,8 +1,8 @@
-import LandingPage from '../LandingPage/LandingPage';
+import LoginPage from '../LoginPage/LoginPage';
 import Favorites from './Favorites/Favorites';
 import MyGoals from './MyGoals/MyGoals';
 import Preferences from './Preferences/Preferences';
 import Profile from './Profile';
 import SomethingElse from './SomethingElse/SomethingElse';
 
-export { Favorites, LandingPage, MyGoals, Preferences, Profile, SomethingElse };
+export { Favorites, LoginPage as LandingPage, MyGoals, Preferences, Profile, SomethingElse };
