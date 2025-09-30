9fb58b2b9d623b217ff11636cb506e8610966793
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-09-30 15:34:11 +0300

    OPHJOD-1917: Fix the wrong type of competence being imported in tool

--- src/routes/Tool/ProfileImportExport.tsx
+++ src/routes/Tool/ProfileImportExport.tsx
@@ -177,9 +177,9 @@ const CompetenceImport = () => {
         .filter((osaaminen) => {
           return mappedSelectedCompetences.some((msc) => {
             if (msc.tyyppi === 'MUU_OSAAMINEN') {
-              return msc.id.includes(osaaminen.osaaminen.uri);
+              return msc.id.includes(osaaminen.osaaminen.uri) && osaaminen.lahde.tyyppi === 'MUU_OSAAMINEN';
             } else if (osaaminen.lahde.id) {
-              return msc.id.includes(osaaminen.lahde.id);
+              return msc.id.includes(osaaminen.lahde.id) && msc.tyyppi === osaaminen.lahde.tyyppi;
             }
             return false;
           });
