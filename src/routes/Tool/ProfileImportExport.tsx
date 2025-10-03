8367f7d69d7c129a010212e64b6143d0d976144d
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-10-03 16:15:47 +0300

    OPHJOD-2405: Dont reset kiinnostukset after import

--- src/routes/Tool/ProfileImportExport.tsx
+++ src/routes/Tool/ProfileImportExport.tsx
@@ -172,6 +172,7 @@ const CompetenceImport = () => {
           kuvaus: k.kuvaus,
           tyyppi: 'KIINNOSTUS' as OsaaminenLahdeTyyppi,
         })),
+      ...storeKiinnostukset.filter((k) => k.tyyppi === 'KARTOITETTU'),
     ];
 
     const toBeImportedSkills = [
