135fd5f42d358403bdfd070c0da20c5712768548
Sauli Anto <sauli.anto@gofore.com>
2025-10-29 15:02:37 +0200

    OPHJOD-2619: Enhance opportunity print view

--- src/components/CompareTable/CompareCompetencesTableRow.tsx
+++ src/components/CompareTable/CompareCompetencesTableRow.tsx
@@ -1,6 +1,5 @@
 import { components } from '@/api/schema';
 import { getLocalizedText } from '@/utils';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export interface CompareCompetencesTableRowData {
@@ -16,18 +15,22 @@ interface CompareCompetencesTableRowProps {
 }
 
 export const CompareCompetencesTableRow = ({ row, className }: CompareCompetencesTableRowProps) => {
-  const title = React.useMemo(() => getLocalizedText(row.kuvaus), [row.kuvaus]);
   const { t } = useTranslation();
 
   return (
     <tr className={className}>
-      <td className="w-full pl-5 pr-7 py-3 text-heading-5 hyphens-auto first-letter:uppercase" title={title}>
+      <td className="w-full pl-5 pr-7 py-3 text-heading-5 hyphens-auto first-letter:uppercase">
         {getLocalizedText(row.nimi)}
       </td>
 
       <td className="justify-items-center pr-5">
         {row.profiili && (
-          <div title={title} role="img" aria-label={t('found')} className="size-4 bg-secondary-1 rounded-full" />
+          <>
+            <div role="img" aria-label={t('found')} className="size-4 bg-secondary-1 rounded-full" />
+            <div aria-hidden className="hidden print:block">
+              {t('found')}
+            </div>
+          </>
         )}
       </td>
     </tr>
