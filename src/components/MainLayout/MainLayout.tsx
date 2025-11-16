6076bf4de74b589fbc535f19344430ad4d0684ae
Sauli Anto <sauli.anto@gofore.com>
2025-11-16 22:37:41 +0200

    OPHJOD-2681: Fix service bar and note stack

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -11,7 +11,7 @@ export const MainLayout = ({ children, navChildren }: MainLayoutProps) => {
 
   return (
     <div
-      className="mx-auto grid w-full max-w-[1140px] grow grid-cols-3 gap-6 px-5 pb-6 pt-8 sm:px-6 print:p-0"
+      className="mx-auto grid w-full max-w-[1140px] grow grid-cols-3 gap-6 px-5 pb-6 pt-11 sm:px-6 print:p-0"
       data-testid="main-layout"
     >
       <Breadcrumb />
--- src/components/Toaster/Toaster.tsx
+++ src/components/Toaster/Toaster.tsx
@@ -1,4 +1,4 @@
-import { Toast } from '@jod/design-system';
+import { Toast, useNoteStack } from '@jod/design-system';
 import { Toast as ReactHotToast, useToaster } from 'react-hot-toast/headless';
 import './toaster.css';
 
@@ -8,15 +8,18 @@ export const Toaster = () => {
   const { toasts, handlers } = useToaster({ duration: 4000 });
   const { startPause, endPause } = handlers;
   const safeToasts = toasts.filter((toast): toast is SafeToast => typeof toast.message === 'string');
-  // Header 48px + 16px padding = 64px
-  // Service bar 40px when open, 4px when closed (scrollY > 0)
+  const { permanentNotesHeight } = useNoteStack();
   // Margin to edges is 32px according to design
-  const topClass = window.scrollY > 0 ? 'top-[100px]' : 'top-[136px]';
+  // navbar + service bar (closed) + permanent notes + 32px
+  const top = `${64 + 4 + permanentNotesHeight + 32}px`;
 
   return (
     <div
       role="alert"
-      className={`fixed ${topClass} right-7 z-60 flex flex-col gap-4 print:hidden`}
+      className={`fixed right-7 z-60 flex flex-col gap-4 print:hidden`}
+      style={{
+        top,
+      }}
       onMouseEnter={startPause}
       onMouseLeave={endPause}
     >
