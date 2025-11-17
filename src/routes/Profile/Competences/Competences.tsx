fea48391fc5e8ab5abb1ecae6365c4a26f6d9edf
Henri Kuokkanen <henri.kuokkanen@sleek.works>
2025-11-17 09:25:46 +0200

    OPHJOD-2664: Add additional info texts

--- src/routes/Profile/Competences/Competences.tsx
+++ src/routes/Profile/Competences/Competences.tsx
@@ -8,6 +8,7 @@ import { GroupBySource } from '@/routes/Profile/Competences/GroupBySource';
 import type { CompetencesLoaderData } from '@/routes/Profile/Competences/loader';
 import { sortByProperty } from '@/utils';
 import { Button, Modal, useMediaQueries } from '@jod/design-system';
+import { JodInfo } from '@jod/design-system/icons';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLoaderData } from 'react-router';
@@ -80,6 +81,12 @@ const Competences = () => {
       <title>{title}</title>
       <ProfileSectionTitle type="OSAAMISENI" title={title} />
       <p className="mb-5 text-body-lg">{t('profile.competences.description')}</p>
+      <div className="flex bg-bg-gray-2 rounded-md max-w-xl py-5 pl-4 pr-6 items-start gap-4 text-secondary-1-dark-2 text-heading-4 mb-8">
+        <div>
+          <JodInfo />
+        </div>
+        <div>{t('profile.competences.info')}</div>
+      </div>
 
       <div>
         {!lg && (
