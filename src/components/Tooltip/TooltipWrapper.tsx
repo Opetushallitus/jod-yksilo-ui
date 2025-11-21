e37c5bc4974f1dec1aacad8b561530e10e0bee1c
Sami Korpela <sami.korpela@gofore.com>
2025-11-21 13:48:40 +0200

    OPHJOD-2638: Update tooltip a11y

--- src/components/Tooltip/TooltipWrapper.tsx
+++ src/components/Tooltip/TooltipWrapper.tsx
@@ -1,5 +1,6 @@
 import { Tooltip, TooltipContent, TooltipTrigger, useMediaQueries } from '@jod/design-system';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const TooltipWrapper = ({
   children,
@@ -15,6 +16,9 @@ export const TooltipWrapper = ({
   halfWidth?: boolean;
 }) => {
   const { sm } = useMediaQueries();
+  const contentId = React.useId();
+  const { t } = useTranslation();
+
   let sizeClassName = '';
   if (halfWidth) {
     sizeClassName = sm ? ' w-1/2' : 'max-w-fit mr-11';
@@ -23,13 +27,23 @@ export const TooltipWrapper = ({
   return (
     <Tooltip open={tooltipOpen} placement={tooltipPlacement} data-testid="tooltip">
       <TooltipContent
+        id={contentId}
         className={`bg-black text-white rounded-xl p-5 z-50 text-body-sm sm:text-body-md font-arial ${sizeClassName}`}
         data-testid="tooltip-content"
       >
         {tooltipContent}
       </TooltipContent>
       <TooltipTrigger asChild>
-        <div data-testid="tooltip-trigger">{children}</div>
+        <div
+          data-testid="tooltip-trigger"
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+          tabIndex={0}
+          role="img"
+          aria-roledescription={t('tooltip.info')}
+          aria-describedby={contentId}
+        >
+          {children}
+        </div>
       </TooltipTrigger>
     </Tooltip>
   );
