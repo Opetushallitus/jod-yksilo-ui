a68abf3fbb28230262d2232f52572373d971f00a
Jussi Sirén <jussi.siren@sleek.works>
2025-12-01 14:00:07 +0200

    OPHJOD-2764: Refactor Tool component to conditionally render opportunities list and improve data fetching in loader

--- src/routes/Tool/loader.ts
+++ src/routes/Tool/loader.ts
@@ -16,6 +16,19 @@ export type ToolLoaderData = {
   };
 } & CompetencesLoaderData;
 
+const getKiinnostukset = async (
+  request: Request,
+): Promise<{
+  kiinnostukset: components['schemas']['OsaaminenDto'][];
+  kiinnostuksetVapaateksti?: components['schemas']['LokalisoituTeksti'];
+}> => {
+  const { data } = await client.GET('/api/profiili/kiinnostukset/osaamiset', { signal: request.signal });
+  const kiinnostukset = await osaamisetService.find(data?.kiinnostukset);
+  const kiinnostuksetVapaateksti = data?.vapaateksti;
+
+  return { kiinnostukset, kiinnostuksetVapaateksti };
+};
+
 export default (async ({ request, context }): Promise<ToolLoaderData> => {
   const state = useToolStore.getState();
   const isLoggedIn = !!context;
@@ -34,20 +47,18 @@ export default (async ({ request, context }): Promise<ToolLoaderData> => {
 
   // Load tyomahdollisuudet and ehdotukset if they are not already loaded
   if (state.tyomahdollisuudet.length === 0 || languageHasChanged) {
-    await state.updateEhdotuksetAndTyomahdollisuudet(isLoggedIn, languageHasChanged);
+    //We don't need to await this
+    state.updateEhdotuksetAndTyomahdollisuudet(isLoggedIn, languageHasChanged);
   }
 
   // Load suosikit if the user is logged in
   if (isLoggedIn) {
-    const suosikitResponse = await client.GET('/api/profiili/suosikit', { signal: request.signal });
+    const [suosikitResponse, competenceLoaderData, { kiinnostukset, kiinnostuksetVapaateksti }] = await Promise.all([
+      client.GET('/api/profiili/suosikit', { signal: request.signal }),
+      getCompetenceData(request, context),
+      getKiinnostukset(request),
+    ]);
     state.setSuosikit(suosikitResponse.data ?? []);
-
-    // Get competence data from profile
-    const competenceLoaderData = await getCompetenceData(request, context);
-
-    const { data } = await client.GET('/api/profiili/kiinnostukset/osaamiset', { signal: request.signal });
-    const kiinnostukset = await osaamisetService.find(data?.kiinnostukset);
-    const kiinnostuksetVapaateksti = data?.vapaateksti;
 
     return { isLoggedIn, kiinnostukset, filters: {}, kiinnostuksetVapaateksti, ...competenceLoaderData };
   } else {
