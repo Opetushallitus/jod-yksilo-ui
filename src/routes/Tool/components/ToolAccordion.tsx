365d4f5fb61e57796f7f9b2a769f1e9b6bd7f812
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-12-04 12:53:41 +0200

    OPHJOD-2829: Store and use the last visited tool tab on mobile when navigating

--- src/routes/Tool/components/ToolAccordion.tsx
+++ src/routes/Tool/components/ToolAccordion.tsx
@@ -1,3 +1,4 @@
+import { hyphenize } from '@/utils';
 import { Accordion } from '@jod/design-system';
 import React from 'react';
 
@@ -22,30 +23,23 @@ const ToolAccordion = ({
   const isControlled = controlledIsOpen !== undefined;
   const isOpen = isControlled ? controlledIsOpen : internalIsOpen;
   const setIsOpen = isControlled && controlledSetIsOpen ? controlledSetIsOpen : setInternalIsOpen;
-  const id = title.toLocaleLowerCase().replace(/\s+/g, '-');
+  const id = hyphenize(title);
   const triggerId = `accordion-${id}`;
   const contentId = `accordion-${id}-content`;
 
   return (
     <div className="bg-white rounded py-6 px-5" ref={ref} data-testid={testId}>
       <Accordion
-        ariaLabel={title}
         isOpen={isOpen}
         caretPosition="top"
         triggerId={triggerId}
         ariaControls={contentId}
         setIsOpen={setIsOpen}
-        title={
-          <>
-            <span className="flex flex-col text-left cursor-pointer w-full text-heading-3">{title}</span>
-            {!isOpen && (
-              <span className="font-arial text-body-sm text-secondary-gray text-pretty hyphens-auto">
-                {description}
-              </span>
-            )}
-          </>
-        }
+        title={title}
         initialState={false}
+        collapsedContent={
+          <span className="font-arial text-body-sm text-secondary-gray text-pretty hyphens-auto">{description}</span>
+        }
         testId="tool-accordion-button"
       >
         <section id={contentId} aria-labelledby={triggerId}>
