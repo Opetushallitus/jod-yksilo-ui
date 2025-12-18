89ced05cb9e9612a74f563784495430be57a44af
Jussi Sirén <jussi.siren@sleek.works>
2025-12-18 14:08:28 +0200

    OPHJOD-2899: Refactor loader to conditionally fetch jakolinkit based on feature flag

--- src/routes/Profile/Preferences/loader.ts
+++ src/routes/Profile/Preferences/loader.ts
@@ -3,10 +3,14 @@ import type { components } from '@/api/schema';
 import i18n, { type LangCode } from '@/i18n/config';
 import { sortByProperty } from '@/utils';
 import { getCodesetValue } from '@/utils/codes/codes';
+import { isFeatureEnabled } from '@/utils/features';
 import type { LoaderFunction } from 'react-router';
 
 const loader = (async ({ request }) => {
-  const { data: jakolinkit = [] } = await client.GET('/api/profiili/jakolinkki', { signal: request.signal });
+  const { data: jakolinkit = [] } = isFeatureEnabled('JAKOLINKKI')
+    ? await client.GET('/api/profiili/jakolinkki', { signal: request.signal }).catch(() => ({ data: [] }))
+    : { data: [] };
+
   const { data: yksiloData } = await client.GET('/api/profiili/yksilo/tiedot-ja-luvat', { signal: request.signal });
   const kotikuntaLabel = yksiloData?.kotikunta
     ? await getCodesetValue('kunta', yksiloData?.kotikunta, i18n.language as LangCode)
