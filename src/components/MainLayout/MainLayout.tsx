123629213e35de0a50de7ac487a52fe59d6657d8
Sauli Anto <sauli.anto@gofore.com>
2025-12-29 12:00:56 +0200

    OPHJOD-2864: Fix CV printing

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -12,7 +12,7 @@ export const MainLayout = ({ children, navChildren, hideBreadcrumb = false }: Ma
 
   return (
     <div
-      className="mx-auto grid w-full max-w-[1140px] grow grid-cols-3 gap-6 px-5 pb-6 pt-11 sm:px-6 print:p-0"
+      className="mx-auto grid w-full max-w-[1140px] grow grid-cols-3 gap-6 px-5 pb-6 pt-11 sm:px-6 print:flex print:p-0"
       data-testid="main-layout"
     >
       {!hideBreadcrumb && <Breadcrumb />}
