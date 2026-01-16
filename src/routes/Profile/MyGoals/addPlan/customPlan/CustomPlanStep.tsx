36d005e1d20ff855682b6b260164b3bd96bca275
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2026-01-16 17:52:46 +0200

    OPHJOD-3006: Create edit modal for custom goals plans

---
+++ src/routes/Profile/MyGoals/addPlan/customPlan/CustomPlanStep.tsx
@@ -0,0 +1,36 @@
+import { InputField, Textarea } from '@jod/design-system';
+import { useFormContext } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import type { OmaSuunnitelmaForm } from './AddOrEditCustomPlanModal';
+
+const CreateCustomPlanStep = () => {
+  const { t, i18n } = useTranslation();
+
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<OmaSuunnitelmaForm>();
+
+  return (
+    <div className="mb-2">
+      <p className="text-body-sm-mobile sm:text-body-sm mt-3">{t('profile.my-goals.add-custom-plan-description')}</p>
+      <div className="mt-6">
+        <InputField
+          label={t('profile.my-goals.custom-plan-name')}
+          requiredText={t('required')}
+          errorMessage={errors?.nimi?.[i18n.language]?.message}
+          {...register(`nimi.${i18n.language}`)}
+        />
+      </div>
+      <div className="mt-6">
+        <Textarea
+          label={t('profile.my-goals.custom-plan-description')}
+          errorMessage={errors?.kuvaus?.[i18n.language]?.message}
+          {...register(`kuvaus.${i18n.language}`)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CreateCustomPlanStep;
