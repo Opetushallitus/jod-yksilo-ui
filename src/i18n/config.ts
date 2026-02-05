7ccf06d73ca7c9a46f952841045ae6789d99f3a3
Jussi Sirén <jussi.siren@sleek.works>
2026-02-05 13:48:52 +0200

    OPHJOD-3094: Enable English in production

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -10,9 +10,7 @@ import translationSv from './sv/translation.json';
 export const LANGUAGE_VALUES = ['fi', 'sv', 'en'] as const;
 export type LanguageValue = (typeof LANGUAGE_VALUES)[number];
 export type LangCode = 'fi' | 'sv' | 'en';
-export const supportedLanguageCodes: LangCode[] =
-  /* eslint-disable sonarjs/todo-tag */
-  window.location.hostname === 'osaamispolku.fi' ? ['fi', 'sv'] : ['fi', 'sv', 'en']; // TODO: Set fi, sv, en when translations are ready
+export const supportedLanguageCodes: LangCode[] = ['fi', 'sv', 'en'];
 export const defaultLang = 'fi';
 
 export const langLabels = {
