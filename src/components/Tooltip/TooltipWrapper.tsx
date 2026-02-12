60ec41961c872712e80032490504050ddbffa0b1
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2026-02-12 10:56:23 +0200

    OPHJOD-3105: Update ammatti opportunity tooltip and update TooltipWrapper to match design

--- src/components/Tooltip/TooltipWrapper.tsx
+++ src/components/Tooltip/TooltipWrapper.tsx
@@ -1,4 +1,4 @@
-import { Tooltip, TooltipContent, TooltipTrigger, useMediaQueries } from '@jod/design-system';
+import { tidyClasses as tc, Tooltip, TooltipContent, TooltipTrigger } from '@jod/design-system';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -7,43 +7,47 @@ export const TooltipWrapper = ({
   tooltipOpen,
   tooltipContent,
   tooltipPlacement = 'bottom',
-  halfWidth = false,
+  className = '',
 }: {
   children: React.ReactNode;
   tooltipContent: React.ReactNode;
   tooltipOpen?: boolean;
   tooltipPlacement?: React.ComponentProps<typeof Tooltip>['placement'];
-  halfWidth?: boolean;
+  className?: string;
 }) => {
-  const { sm } = useMediaQueries();
   const contentId = React.useId();
   const { t } = useTranslation();
-
-  let sizeClassName = '';
-  if (halfWidth) {
-    sizeClassName = sm ? ' w-1/2' : 'max-w-fit mr-11';
-  }
 
   return (
     <Tooltip open={tooltipOpen} placement={tooltipPlacement} data-testid="tooltip">
       <TooltipContent
         id={contentId}
-        className={`max-w-[290px] bg-black text-white rounded-xl p-5 z-50 text-body-sm sm:text-body-md font-arial ${sizeClassName}`}
+        className={tc([
+          'font-arial',
+          'text-body-sm',
+          'sm:text-body-md',
+          'sm:max-w-[296px]',
+          'max-w-[256px]',
+          'p-4',
+          'bg-primary-gray',
+          'text-white',
+          'rounded-md',
+          'z-50',
+          className,
+        ])}
         data-testid="tooltip-content"
       >
         {tooltipContent}
       </TooltipContent>
-      <TooltipTrigger asChild>
-        <div
-          data-testid="tooltip-trigger"
-          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-          tabIndex={0}
-          role="img"
-          aria-roledescription={t('tooltip.info')}
-          aria-describedby={contentId}
-        >
-          {children}
-        </div>
+      <TooltipTrigger
+        data-testid="tooltip-trigger"
+        className="select-none"
+        tabIndex={0}
+        aria-roledescription={t('tooltip.info')}
+        aria-describedby={contentId}
+        aria-label={typeof tooltipContent === 'string' ? tooltipContent : t('tooltip.info')}
+      >
+        {children}
       </TooltipTrigger>
     </Tooltip>
   );
