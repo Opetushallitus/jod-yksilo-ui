8b9dd2d685ab38c96806bf479f31438d6b0b1660
Henri Kuokkanen <henri.kuokkanen@sleek.works>
2026-02-20 13:32:19 +0200

    OPHJOD-3120: Update additional resources in tool

--- src/components/HelpingToolsContent/index.ts
+++
@@ -1,2 +0,0 @@
-export { HelpingToolLinkItem, HelpingToolProfileLinkItem } from './HelpingToolLinkItem';
-export type { HelpingToolLinkItemProps, HelpingToolProfileLinkItemProps } from './HelpingToolLinkItem';
