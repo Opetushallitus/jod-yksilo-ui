d5337eedcc8f4a352b13402441944a023941670f
Jussi Sirén <jussi.siren@sleek.works>
2026-02-24 10:34:00 +0200

    OPHJOD-3155: Ensure feature flags are always loaded before router creation
    - Notifications loading errors no longer affect router initialization.

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -14,7 +14,17 @@ interface Notification {
 const notifications: Notification[] = [] as Notification[];
 
 export const loadNotifications = async () => {
-  notifications.push(...(await (await fetch(`${import.meta.env.BASE_URL}config/notifications.json`)).json()));
+  try {
+    const loadedNotifications = await fetch(`${import.meta.env.BASE_URL}config/notifications.json`).then((res) =>
+      res.json(),
+    );
+    notifications.push(...loadedNotifications);
+  } catch (error) {
+    // It's safe to ignore this error.
+    // If notification loading fails, the app will continue to work with no notifications.
+    /* eslint-disable-next-line no-console */
+    console.warn('Failed to load notifications, using defaults', error);
+  }
 };
 
 export const getNotifications = () => notifications;
