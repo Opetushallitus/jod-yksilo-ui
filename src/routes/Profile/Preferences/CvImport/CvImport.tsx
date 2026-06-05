6cd69a92752b7a8b91c190246599085f49caf442
Sami Korpela <sami.korpela@gofore.com>
2026-06-05 09:02:51 +0300

    OPHJOD-3413: Add AiInfo for CV-import

--- src/routes/Profile/Preferences/CvImport/CvImport.tsx
+++ src/routes/Profile/Preferences/CvImport/CvImport.tsx
@@ -1,11 +1,42 @@
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
-import { Button } from '@jod/design-system';
+import { AiInfoButton, Button } from '@jod/design-system';
+import { JodOpenInNew } from '@jod/design-system/icons';
 
+import { AnchorLink } from '@/components';
 import { useModal } from '@/hooks/useModal';
 import { useSessionGuardedAction } from '@/hooks/useSessionGuardedAction';
 
 import CvImportWizard from './CvImportWizard';
+
+const TooltipContent = () => {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+
+  return (
+    <div className="flex max-w-[290px] flex-col gap-3 text-body-xs leading-5">
+      <p className="font-semibold">{'Osaamisprofiilin täyttö tekoälyn avulla.'}</p>
+      <p>{t('ai-info-tooltip.description-cv-import')}</p>
+      <div>
+        <Trans
+          i18nKey="ai-info-tooltip.description-summary"
+          components={{
+            Icon: <JodOpenInNew size={18} ariaLabel={t('common:external-link')} />,
+            CustomLink: (
+              <AnchorLink
+                href={`/${language}/${t('common:slugs.ai-usage')}`}
+                className="inline-flex underline"
+                target="_blank"
+              />
+            ),
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export const CvImport = () => {
   const { t } = useTranslation();
@@ -15,7 +46,12 @@ export const CvImport = () => {
 
   return (
     <div>
-      <h3 className="mb-3 text-heading-3-mobile sm:text-heading-3">{t('preferences.cv-import.title')}</h3>
+      <h3 className="mb-3 text-heading-3-mobile sm:text-heading-3">
+        {t('preferences.cv-import.title')}
+        <div className="ml-2 inline *:align-top print:hidden">
+          <AiInfoButton tooltipContent={<TooltipContent />} ariaLabel={t('ai-info-tooltip.aria-description')} />
+        </div>
+      </h3>
       <p className="mb-7 font-arial">{t('preferences.cv-import.description')}</p>
 
       <Button
