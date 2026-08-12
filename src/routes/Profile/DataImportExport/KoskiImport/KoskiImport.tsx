4d69394c6814a978fcd70bb3e7203e52e90a6bbe
Sami Korpela <sami.korpela@gofore.com>
2026-08-12 14:54:48 +0300

    OPHJOD-3449: Update Goals to show only one at a time

--- src/routes/Profile/DataImportExport/KoskiImport/KoskiImport.tsx
+++ src/routes/Profile/DataImportExport/KoskiImport/KoskiImport.tsx
@@ -1,12 +1,17 @@
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 import { Button } from '@jod/design-system';
 
+import { AnchorLink } from '@/components';
+import { ImportInfoBox } from '@/components/ImportInfoBox/ImportInfoBox';
 import { useSessionGuardedAction } from '@/hooks/useSessionGuardedAction';
 import { useKoskiImport } from '@/routes/Profile/EducationHistory/useKoskiImport';
 
 const KoskiImport = () => {
-  const { t } = useTranslation();
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
   const guardedAction = useSessionGuardedAction();
   const { openImportStartModal } = useKoskiImport();
 
@@ -14,7 +19,6 @@ const KoskiImport = () => {
     <div>
       <h3 className="mb-3 text-heading-3-mobile sm:text-heading-3">{t('preferences.koski-import.title')}</h3>
       <p className="mb-6 font-arial">{t('preferences.koski-import.description')}</p>
-
       <Button
         label={t('preferences.koski-import.button-label')}
         size="sm"
@@ -22,6 +26,20 @@ const KoskiImport = () => {
         ariaHaspopup="dialog"
         onClick={guardedAction(openImportStartModal)}
       />
+
+      <ImportInfoBox>
+        <Trans
+          i18nKey="preferences.koski-import.result-info"
+          components={{
+            EducationHistoryLink: (
+              <AnchorLink
+                href={`/yksilo/${language}/${t('slugs.profile.index')}/${t('slugs.profile.competences')}/${t('slugs.profile.education-history')}`}
+                className="inline-flex underline"
+              />
+            ),
+          }}
+        />
+      </ImportInfoBox>
     </div>
   );
 };
