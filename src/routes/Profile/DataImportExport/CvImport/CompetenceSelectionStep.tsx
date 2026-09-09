8338df64b4d1a6925c42e6b32b0421de299bc634
Sami Korpela <sami.korpela@gofore.com>
2026-09-09 09:27:42 +0300

    OPHJOD-3508: WIP3

--- src/routes/Profile/DataImportExport/CvImport/CompetenceSelectionStep.tsx
+++ src/routes/Profile/DataImportExport/CvImport/CompetenceSelectionStep.tsx
@@ -1,5 +1,7 @@
 import { Trans, useTranslation } from 'react-i18next';
 
+import { Accordion } from '@jod/design-system';
+
 import type { ExperienceTableRowData } from '@/components';
 import { getLocalizedText } from '@/utils';
 
@@ -12,30 +14,51 @@ interface SectionProps {
 }
 
 const Section = ({ titleText, headerText, rows }: SectionProps) => {
+  const { t } = useTranslation();
   return (
     <div>
       <h2 className="mb-6 text-heading-2-mobile sm:text-heading-2">{titleText}</h2>
       {rows && (
         <div className="px-4">
-          <div className="flex flex-row justify-between border-b-2 border-primary-5-light-2">
+          <div className="mb-6 flex w-full flex-row justify-between border-b-2 border-primary-5-light-2">
             <p className="text-heading-5-mobile sm:text-heading-5">{headerText}</p>
-            <p>TODO: Osaamiset</p>
+            <p>{t('preferences.cv-import.competence-selection.competences')}</p>
           </div>
           {rows.map((row) => (
             <div key={row.key} className="py-4">
               <h3 className="font-poppins text-heading-4-mobile sm:text-heading-4">{getLocalizedText(row.nimi)}</h3>
-              <div className="flex flex-col gap-7">
+              <div className="flex flex-col gap-7 pt-6">
                 {row.subrows?.map((subrow) => (
                   <div key={subrow.key} className="pl-7">
-                    <div className="flex flex-row justify-between">
-                      <h4 className="text-heading-5-mobile sm:text-heading-5">{getLocalizedText(subrow.nimi)}</h4>
-                      <p>TODO: Valittu xx/yy</p>
-                    </div>
-                    <div className="mt-3 border-l-4 border-border-gray bg-white">
-                      <div className="pl-3">
-                        <p>TODO: Valitse osaamiset</p>
+                    <Accordion
+                      ariaLabel={getLocalizedText(subrow.nimi)}
+                      title={
+                        <div className="flex flex-row justify-between">
+                          <h4 className="text-heading-5-mobile sm:text-heading-5">{getLocalizedText(subrow.nimi)}</h4>
+                          <p className="text-body-md-mobile sm:text-body-md">
+                            {t('preferences.cv-import.competence-selection.selected-competences', {
+                              // TODO: correct values
+                              selected: 0,
+                              total: 99,
+                            })}
+                          </p>
+                        </div>
+                      }
+                    >
+                      <div className="mt-3 border-l-4 border-border-gray bg-white">
+                        <div className="flex flex-col py-2 pl-3">
+                          <p className="text-heading-4-mobile sm:text-heading-4">
+                            {t('preferences.cv-import.competence-selection.select-competences')}
+                          </p>
+                          <p className="text-help">{t('preferences.cv-import.competence-selection.add-competence')}</p>
+                          <div>
+                            {subrow.osaamiset.map((osaaminen) => (
+                              <div key={osaaminen.id}>{getLocalizedText(osaaminen.nimi)}</div>
+                            ))}
+                          </div>
+                        </div>
                       </div>
-                    </div>
+                    </Accordion>
                   </div>
                 ))}
               </div>
@@ -52,7 +75,6 @@ interface CompetenceSelectionStepProps {
 }
 export const CompetenceSelectionStep = ({ convertedData }: CompetenceSelectionStepProps) => {
   const { t } = useTranslation();
-  //
   return (
     <div className="box-content flex max-w-modal-content flex-col gap-7 px-5 font-arial md:max-w-none md:px-9">
       <p>
